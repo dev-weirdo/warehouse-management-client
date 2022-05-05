0c3b2afa44c018067b739ab3973c8670925ca710
dev-weirdo <dev_weirdo@protonmail.com>
2022-05-05 21:59:17 +0600

    Login page added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-            <p>Login</p>
+        <div className='bg-lime-300 my-3 mx-1 md:m-8 rounded-lg h-screen flex flex-col justify-center'>
+            <form className='max-w-[380px] w-full mx-auto bg-lime-500 p-8 rounded-lg'>
+                <p className='text-center text-3xl'>Login</p>
+                <div className='border-black border my-2'></div>
+                <div className='flex flex-col'>
+                    <label>Email</label>
+                    <input className='mt-2 p-2 bg-gray-200 focus:bg-white outline-none rounded-lg' type="email" required />
+                </div>
+                <div className='flex flex-col mt-2'>
+                    <label>Password</label>
+                    <input className='mt-2 p-2 bg-gray-200 focus:bg-white outline-none rounded-lg' type="password" required />
+                </div>
+                <button className='bg-slate-800 hover:bg-slate-900 text-white w-full my-5 py-2 rounded-md'>Sign In</button>
+            </form>
         </div>
     );
 };
