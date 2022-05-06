566d7af6a1bc125789fb1f89f54b486f3adc006e
dev-weirdo <dev_weirdo@protonmail.com>
2022-05-06 20:36:22 +0600

    Handled Google Signin

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Inventory from "./components/Inventory/Inventory";
 import InventoryItem from "./components/InventoryItem/InventoryItem";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
+import RequireAuth from "./components/RequireAuth/RequireAuth";
 import Footer from "./components/Shared/Footer/Footer";
 import Header from "./components/Shared/Header/Header";
 import Home from "./pages/Home/Home";
@@ -15,9 +16,13 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/inventory" element={<Inventory></Inventory>}></Route>
+        <Route path="/inventory" element={<RequireAuth>
+          <Inventory></Inventory>
+        </RequireAuth>}></Route>
         <Route path="/inventory/:id" element={<InventoryItem></InventoryItem>}></Route>
-        <Route path="/addnewitem" element={<AddNewItem></AddNewItem>}></Route>
+        <Route path="/addnewitem" element={<RequireAuth>
+          <AddNewItem></AddNewItem>
+        </RequireAuth>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/register" element={<Register></Register>}></Route>
       </Routes>
--- src/components/SocialLogin/SocialLogin.js
+++ src/components/SocialLogin/SocialLogin.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import googleLogo from '../../assets/google.png'
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const from = location?.state?.from?.pathname || '/';
+
+
+    if (user) {
+        navigate(from, { replace: true });
+    }
+
     return (
         <div>
             <div className='flex items-center'>
@@ -10,7 +24,7 @@ const SocialLogin = () => {
                 <hr className='border-black w-5/12' />
             </div>
             <div className='mt-3'>
-                <p className='flex cursor-pointer justify-center items-center mx-auto text-white bg-slate-800 hover:bg-slate-900 p-1 rounded-md'><img className='w-4 h-4 mr-1' src={googleLogo} alt="" />Continue with Google</p>
+                <p onClick={() => signInWithGoogle()} className='flex cursor-pointer justify-center items-center mx-auto text-white bg-slate-800 hover:bg-slate-900 p-1 rounded-md'><img className='w-4 h-4 mr-1' src={googleLogo} alt="" />Continue with Google</p>
             </div>
         </div>
     );
